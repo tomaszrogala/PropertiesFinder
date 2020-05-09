c528997acd30fc98fd9675b9d007a52351336fed
PiotrTybura <piotr.tybura@gmail.com>
2020-05-09 12:15:41 +0200

    Changed reflection loaded to load all solution libraries irrespectively of CLR loading

--- Application/Program.cs
+++ Application/Program.cs
@@ -2,6 +2,7 @@
 using Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using Utilities;
@@ -65,8 +66,11 @@ namespace SampleApp
             if (!interfaceType.GetTypeInfo().IsInterface)
                 throw new ArgumentException();
 
-            return AppDomain.CurrentDomain.GetAssemblies()
-                  .SelectMany(element => element.GetTypes())
+            var assemblies = new FileInfo(Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path))
+                .Directory.GetFiles().Where(file => file.Extension == ".dll")
+                .Select(dll => Assembly.LoadFile(dll.FullName));
+
+            return assemblies.SelectMany(element => element.GetTypes())
                   .Where(type => interfaceType.IsAssignableFrom(type)
                   && type.GetTypeInfo().IsClass);
         }
